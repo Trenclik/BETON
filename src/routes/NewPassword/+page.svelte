40ece9404e2cc19c0ff9050e37924a0c45885504
Marat141 <terest141@gmail.com>
2025-03-05 12:09:23 +0100

    Základní-kostra-htmnl--NEMÁME STYLE

---
+++ src/routes/NewPassword/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+<main>
+    <div class="NewPasswordWindow">
+        <h1>Vytvoř si nové heslo</h1>
+        <div class="NewPassInputs">
+            <h3>Nové heslo</h3>
+            <input type="password" placeholder="Nové heslo">
+            <h3>Potvrzení hesla</h3>
+            <input type="password" placeholder="Nové heslo znovu">
+            <input type="submit" value="Resetovat heslo">
+        </div>
+    </div>
+</main>
---
+++ src/routes/Register/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+</script>
+
+<main>
+    <div class="windowForLogin">
+        <h1>Registrace</h1>
+        <div class="inputsValue">
+            <input type="text" placeholder="Jméno">
+            <input type="text" placeholder="Přijmení">
+            <input type="email" placeholder="Email">
+            <input type="password" placeholder="Heslo">
+            <input type="password" placeholder="Znovu Heslo">
+            <input type="submit" value="Registrace">
+            <a href="./AnotherWayToLogin"><button class="AnotherWayToLogin">Přihlásit se jinak</button></a>
+            <!--<a href="./ForgetPassword">Zapomenuté heslo?</a>-->
+        </div>
+    </div>
+</main>
