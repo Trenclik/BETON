900da1fcc10bb64f8500d506d2941a24d60862f1
Marat141 <terest141@gmail.com>
2025-03-05 12:09:23 +0100

    Základní-kostra-htmnl--NEMÁME STYLE

---
+++ src/routes/ForgetPassword/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+</script>
+
+<main>
+    <div class="ForgetPasswordWindow">
+        <h1>Restartování Hesla</h1>
+        <h3>Dostanete Email s instrukcemi</h3>
+        <div class="ForgetPassInputs">
+            <input type="email" placeholder="Vaše Emailová adresa">
+            <input type="submit" value="Odeslat">
+        </div>
+    </div>
+</main>
---
+++ src/routes/VerifyEmail/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+
+<main>
+    <h1>Ověřte svůj email, poslali jsme vám ověření na email:  </h1>
+</main>
