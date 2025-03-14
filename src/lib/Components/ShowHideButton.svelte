83bb5d12b17f590e4962ad7d19f3faeab8d54636
Marat141 <terest141@gmail.com>
2025-03-14 11:37:45 +0100

    Style-nedodelany
    Nedodělaný style zatím nesahat.

--- src/lib/Components/ShowHideButton.svelte
+++ src/lib/Components/ShowHideButton.svelte
@@ -1,31 +1,37 @@
 <script lang="ts">
-    const state = $state({
-        showPassword: false
-    });
+  let { class: customClass } = $props<{ class?: string }>();
 
-    const type = $derived.by(() => state.showPassword ? 'text' : 'password');
+  const state = $state({ showPassword: false });
 
-    function togglePassword() {
-        state.showPassword = !state.showPassword;
-    }
+  const type = $derived.by(() => (state.showPassword ? "text" : "password"));
+
+  function togglePassword() {
+    state.showPassword = !state.showPassword;
+  }
 </script>
 
 <main>
-    <div class="password-container">
-        <input type={type} placeholder="Password">
-        <button 
-            class="show-hide-password" 
-            onclick={togglePassword}
+  <div class="password-container">
+    <input class="PaswwordInput {customClass}" {type} placeholder="Password" />
+    <button class="show-hide-password" onclick={togglePassword}>
+      {#if state.showPassword}
+        <!-- SVG pro view.svg -->
+        <svg
+          version="1.0"
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 512.000000 512.000000"
+          preserveAspectRatio="xMidYMid meet"
         >
-            {#if state.showPassword}
-                <!-- SVG pro view.svg -->
-                <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-                width="32" height="32" viewBox="0 0 512.000000 512.000000"
-                preserveAspectRatio="xMidYMid meet">
-               
-                <g transform="translate(0,512) scale(0.1,-0.1)"
-               fill="rgba(30, 138, 37, 1)" stroke="currentColor" stroke-width="30">
-               <path d="M2370 4184 c-589 -70 -1135 -342 -1715 -855 -203 -179 -537 -540
+          <g
+            transform="translate(0,512) scale(0.1,-0.1)"
+            fill="rgba(30, 138, 37, 1)"
+            stroke="currentColor"
+            stroke-width="30"
+          >
+            <path
+              d="M2370 4184 c-589 -70 -1135 -342 -1715 -855 -203 -179 -537 -540
                -623 -674 -36 -56 -36 -134 0 -190 46 -72 240 -297 378 -438 493 -508 1042
                -864 1560 -1012 225 -64 339 -79 590 -79 251 0 365 15 590 79 607 174 1274
                647 1806 1283 146 174 158 195 158 262 0 67 -12 88 -158 262 -529 631 -1194
@@ -33,26 +39,37 @@
                c471 -71 984 -348 1477 -799 151 -137 448 -454 448 -477 0 -3 -34 -46 -77 -94
                -582 -666 -1244 -1089 -1848 -1180 -128 -19 -362 -19 -490 0 -435 66 -899 303
                -1360 694 -167 141 -565 551 -565 581 0 4 34 47 77 95 578 661 1240 1086 1839
-               1179 122 19 375 19 499 1z"/>
-               <path d="M2420 3564 c-433 -79 -741 -361 -846 -774 -27 -106 -27 -354 0 -460
+               1179 122 19 375 19 499 1z"
+            />
+            <path
+              d="M2420 3564 c-433 -79 -741 -361 -846 -774 -27 -106 -27 -354 0 -460
                97 -380 376 -659 756 -756 106 -27 354 -27 460 0 380 97 659 376 756 756 15
                58 19 110 19 230 0 120 -4 172 -19 230 -95 375 -366 650 -741 752 -68 19 -323
                33 -385 22z m300 -350 c187 -42 358 -179 445 -359 52 -107 68 -189 63 -325 -5
                -129 -30 -216 -90 -318 -42 -72 -158 -188 -230 -230 -211 -124 -485 -124 -696
                0 -72 42 -188 158 -230 230 -124 211 -124 485 0 696 42 72 158 188 230 230
-               151 89 328 116 508 76z"/>
-               </g>
-               </svg>
-               
-            {:else}
-                <!-- SVG pro hide.svg -->
-                <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-                width="32" height="32" viewBox="0 0 512.000000 512.000000"
-                preserveAspectRatio="xMidYMid meet">
-               
-                <g transform="translate(0,512) scale(0.1,-0.1)"
-                fill="rgba(30, 138, 37, 1)" stroke="currentColor" stroke-width="30">
-               <path d="M4260 4470 c-14 -6 -147 -133 -297 -282 l-271 -271 -129 50 c-208 83
+               151 89 328 116 508 76z"
+            />
+          </g>
+        </svg>
+      {:else}
+        <!-- SVG pro hide.svg -->
+        <svg
+          version="1.0"
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 512.000000 512.000000"
+          preserveAspectRatio="xMidYMid meet"
+        >
+          <g
+            transform="translate(0,512) scale(0.1,-0.1)"
+            fill="rgba(30, 138, 37, 1)"
+            stroke="currentColor"
+            stroke-width="30"
+          >
+            <path
+              d="M4260 4470 c-14 -6 -147 -133 -297 -282 l-271 -271 -129 50 c-208 83
                -406 135 -638 170 -184 27 -571 24 -764 -6 -509 -78 -959 -281 -1385 -624
                -135 -108 -391 -366 -500 -502 -118 -149 -264 -368 -272 -410 -11 -60 4 -100
                86 -223 240 -363 584 -709 930 -937 58 -38 107 -70 109 -72 3 -2 -104 -113
@@ -65,55 +82,63 @@
                -95 59 c-332 209 -669 527 -878 829 l-51 74 50 73 c177 256 485 564 746 748
                352 248 768 405 1181 448 124 13 448 5 574 -14z m-198 -634 c62 -16 178 -64
                178 -74 0 -1 -195 -197 -434 -436 l-434 -434 -27 52 c-36 73 -65 191 -65 270
-               1 409 394 721 782 622z"/>
-               <path d="M4137 3462 c-59 -59 -107 -112 -106 -117 0 -6 39 -40 87 -77 205
+               1 409 394 721 782 622z"
+            />
+            <path
+              d="M4137 3462 c-59 -59 -107 -112 -106 -117 0 -6 39 -40 87 -77 205
                -157 475 -445 621 -660 l32 -48 -41 -60 c-172 -254 -471 -557 -735 -746 -452
                -324 -980 -489 -1505 -470 -164 5 -215 11 -383 42 l-89 17 -129 -129 c-93 -93
                -125 -130 -115 -136 28 -16 265 -69 401 -90 187 -29 568 -31 750 -5 645 95
                1183 373 1675 866 176 177 291 316 420 509 129 192 129 209 12 388 -112 170
-               -265 357 -427 520 -137 138 -328 304 -349 304 -6 0 -60 -48 -119 -108z"/>
-               <path d="M3336 2661 l-138 -138 -13 -79 c-16 -92 -73 -214 -134 -287 -94 -113
+               -265 357 -427 520 -137 138 -328 304 -349 304 -6 0 -60 -48 -119 -108z"
+            />
+            <path
+              d="M3336 2661 l-138 -138 -13 -79 c-16 -92 -73 -214 -134 -287 -94 -113
                -238 -198 -375 -222 l-79 -13 -141 -141 c-77 -77 -137 -143 -134 -146 12 -12
                165 -34 239 -35 253 -1 486 98 674 285 193 193 278 396 279 665 1 125 -16 250
-               -32 250 -4 0 -69 -62 -146 -139z"/>
-               </g>
-               </svg>
-            {/if}
-        </button>
-    </div>
+               -32 250 -4 0 -69 -62 -146 -139z"
+            />
+          </g>
+        </svg>
+      {/if}
+    </button>
+  </div>
 </main>
 
 <style>
-    .password-container {
-        position: relative;
-        display: flex;
-        align-items: center;
-        width: fit-content;
-    }
+  .password-container {
+    position: relative;
+    display: flex;
+    align-items: center;
+    width: fit-content;
+  }
 
-    input {
-        padding: 10px;
-        padding-right: 40px; 
-        font-size: 16px;
-        border: 2px solid #ccc;
-        border-radius: 5px;
-        outline: none;
-    }
+  input[type="password"]::placeholder {
+    text-align: center;
+    align-items: center;
+    font-size: 18px;
+  }
+  input{
+    background-color: rgba(255, 255, 255, 1);
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    height: 40px;
+    width: 210px;
+    margin: 0;
+  }
 
-    .show-hide-password {
-        position: absolute;
-        right: 10px;
-        background: transparent;
-        border: none;
-        cursor: pointer;
-        width: 43;
-        height: 33;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        border-radius: 50%;
-        transition: color 0.3s ease; /* Animace změny barvy */
-    }
-
-
+  .show-hide-password {
+    position: absolute;
+    right: 10px;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    width: 43;
+    height: 33;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    transition: color 0.3s ease; /* Animace změny barvy */
+  }
 </style>
