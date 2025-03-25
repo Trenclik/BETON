dfd3664648c3603d3385557aa8c1e3e14f4123a3
Marat141 <terest141@gmail.com>
2025-03-25 09:33:20 +0100

    SettingNewPassword

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,9 +7,10 @@
     <a style="color: green;" href="./SuccesfulRegister">SuccesfulRegister</a>
     <a style="color: green;" href="./Register">Register</a>
     <a style="color: red;" href="./Profile">Profile</a>
-    <a style="color: green;" href="./NewPassword">NewPassword</a>
+    <a style="color: green;" href="./ForgetPassword">ForgetPassword</a>
     <a style="color: green;" href="./Login">Login</a>
     <a style="color: green;" href="./FaildRegister">FaildRegister</a>
     <a style="color: red;" href="./EmailWeb">EmailWeb</a>
     <a style="color: red;" href="./AnotherWayToLogin">AnotherWayToLogin</a>
+    <a href="./SettingNewPassword">SettingNewPassword</a>
 </main>
