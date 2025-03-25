92cd222d1075d0df24c415fa07b835d5c7af46a1
Marat141 <terest141@gmail.com>
2025-03-25 08:43:09 +0100

    Upravy
    Přidal jsem stránku FaildRegister, Odstranil jsem ForgetPassword z důvodu že je to to samé jako NewPassword. Upravil jsem style někde kde to bylo potřeba.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,13 @@
 </script>
 
 <main>
-    <h1>Main Page</h1>
+    <a style="color: green;" href="./VerifyEmail">Verifikace Emailu</a>
+    <a style="color: green;" href="./SuccesfulRegister">SuccesfulRegister</a>
+    <a style="color: green;" href="./Register">Register</a>
+    <a style="color: red;" href="./Profile">Profile</a>
+    <a style="color: green;" href="./NewPassword">NewPassword</a>
+    <a style="color: green;" href="./Login">Login</a>
+    <a style="color: green;" href="./FaildRegister">FaildRegister</a>
+    <a style="color: red;" href="./EmailWeb">EmailWeb</a>
+    <a style="color: red;" href="./AnotherWayToLogin">AnotherWayToLogin</a>
 </main>
